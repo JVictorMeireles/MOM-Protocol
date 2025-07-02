88496fff325c21d30278aae168d9ac9da103e3f3
João Victor Meireles Vieira <joao.victor.meireles07@aluno.ifce.edu.br>
2025-07-02 00:50:00 -0300

    pseudocodigo 1

---
+++ Cliente.py
@@ -0,0 +1,56 @@
+# import org.apache.activemq.ActiveMQConnection
+# import org.apache.activemq.ActiveMQConnectionFactory
+import stomp
+
+# class Produtor:
+  # url
+  # nome_fila
+  
+  # estabelece conexão
+  # cria sessão
+  # cria queue
+  # cria produtor
+  # envia mensagem
+  # retorna mensagem enviada
+
+# class Consumidor:
+  # url
+  # nome_fila
+  
+  # estabelece conexão
+  # cria sessão
+  # cria queue
+  # cria consumidor
+  # recebe mensagem
+  # exibe mensagem recebida
+
+# class Publisher:
+  # url
+  # nome_topico
+
+  # estabelece conexão
+  # cria sessão
+  # cria tópico
+  # cria publisher
+  # publica mensagem
+  # retorna mensagem publicada
+
+# class Subscriber:
+  # url
+  # nome_topico
+
+  # estabelece conexão
+  # cria sessão
+  # cria tópico
+  # cria subscriber
+  # seta listener
+    # recebe mensagem
+
+# class Interface:
+  # cria frame
+  # cria botões
+  # cria campo de texto
+  # cria botão de enviar
+  # cria botão de subscribe
+  # cria botão de modo tópico
+  # cria botão de modo fila
